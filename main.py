9f10c9638c6e65b9c26efc8f6465c8dca8b287ac
heavenyoung <jacksonou3@gmail.com>
2026-03-08 10:04:38 +0300

    Deleted logger, instead him output to tne console

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import argparse
 import sys
-from logger import logger
 
 from profile_store import ProfileStore
 from adapter_applier import AdapterApplier
@@ -8,7 +7,7 @@ from adapter_applier import AdapterApplier
 def cmd_list(store: ProfileStore):
     profiles = store.list_profiles()
     if not profiles:
-        logger.warning('Нет сохраненных профилей')
+        print('No saved profiles')
         return
     for name, data in profiles.items():
         print(f'{name} - {data}')
@@ -17,40 +16,41 @@ def cmd_list(store: ProfileStore):
 def cmd_apply(adapter_name: str, profile_name: str, store: ProfileStore, applier: AdapterApplier):
     profile = store.get_profile(profile_name)
     if profile is None:
-        logger.error(f'Профиль "{profile_name}" не найден')
+        print(f'Profile {profile_name} was not found')
         sys.exit(1)
 
-    adapter = adapter_name or profile.get('adapter')  # ← из аргумента или из профиля
+    adapter = adapter_name or profile.get('adapter')
     if not adapter:
-        print('Адаптер не указан')
+        print('Adapter not specify')
         sys.exit(1)
 
     applier.apply_profile(adapter, profile)
+    print(f'Adapter {profile_name} successfully set')
 
 
 def cmd_delete(profile_name: str, store: ProfileStore):
-    """Delete a profile by name."""
+    '''Delete a profile by name.'''
     if store.delete_profile(profile_name):
-        logger.info(f'Профиль "{profile_name}" удалён')
+        print(f'Profie successfully {profile_name} deleted')
     else:
-        logger.error(f'Профиль "{profile_name}" не найден')
+        print(f'Profile {profile_name} was not found')
         sys.exit(1)
 
 def main():
-    parser = argparse.ArgumentParser(description='netsnap — смена сетевых профилей')
+    parser = argparse.ArgumentParser(description='netsnap - switching network profiles')
     sub = parser.add_subparsers(dest='command')
 
     # команда: python main.py list
     sub.add_parser('list', help='Список профилей')
 
     # команда: python main.py apply work --adapter "Беспроводная сеть"
-    p_apply = sub.add_parser('apply', help='Применить профиль')
-    p_apply.add_argument('name', help='Имя профиля')
-    p_apply.add_argument('--adapter', default=None, help='Имя адаптера')
+    p_apply = sub.add_parser('apply', help='Apply profile')
+    p_apply.add_argument('name', help='Name profile')
+    p_apply.add_argument('--adapter', default=None, help='Adapter name')
 
     # команда: python main.py delete work
-    p_delete = sub.add_parser('delete', help='Удалить профиль')
-    p_delete.add_argument('name', help='Имя профиля для удаления')
+    p_delete = sub.add_parser('delete', help='Delete profile')
+    p_delete.add_argument('name', help='Profile name for remove')
 
     args = parser.parse_args()
     store = ProfileStore()
