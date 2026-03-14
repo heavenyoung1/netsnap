e8b879dad14f3422298790bbffaeeef0785a2ac8
heavenyoung <jacksonou3@gmail.com>
2026-03-14 18:02:11 +0300

    Method get_adapters now also return display name for adapter, example Wifi. Early i need input name adater - Intel(R) Wi-Fi 6 AX201 160MHz. I chaned it simplyfy, using human reading settings

--- adapter_wmi.py
+++ adapter_wmi.py
@@ -4,6 +4,7 @@ from typing import NamedTuple
 
 class AdapterInfo(NamedTuple):
     name: str
+    adapter: str
     dhcp_enabled: bool
     ip: str
     subnet: str
@@ -18,23 +19,34 @@ class AdapterManager:
 
     def get_adapters(self) -> dict[str, AdapterInfo]:
         '''Returns all active network adapters with their current settings.'''
-        net_adapters = self._wmi.Win32_NetworkAdapterConfiguration
+        _adapter_configurations = self._wmi.Win32_NetworkAdapterConfiguration
+        adapter_configurations = _adapter_configurations(IPEnabled=True)
+        adapters = self._wmi.Win32_NetworkAdapter()
 
-        output = {
-            adapter.Description: AdapterInfo(
-                name=adapter.Description,
-                dhcp_enabled=adapter.DHCPEnabled,
-                ip=adapter.IPAddress[0],
-                subnet=adapter.IPSubnet[0],
-                gateway=(
-                    adapter.DefaultIPGateway[0] if adapter.DefaultIPGateway else None
-                ),
-                dns_enabled=adapter.DNSServerSearchOrder is not None,
-                dns_servers=adapter.DNSServerSearchOrder,
-            )
-            for adapter in net_adapters(IPEnabled=True)
+        name_map = {
+            adapter.Name: adapter.NetConnectionID 
+            for adapter in adapters 
+            if adapter.NetConnectionID
         }
+
+        output = {} 
+        for adapter_config in adapter_configurations:
+            display_name = name_map.get(adapter_config.Description)
+            output[display_name] = AdapterInfo(
+                name=display_name,
+                adapter=adapter_config.Description,
+                dhcp_enabled=adapter_config.DHCPEnabled,
+                ip=adapter_config.IPAddress[0],
+                subnet=adapter_config.IPSubnet[0],
+                gateway=(
+                    adapter_config.DefaultIPGateway[0] if adapter_config.DefaultIPGateway else None
+                ),
+                dns_enabled=adapter_config.DNSServerSearchOrder is not None,
+                dns_servers=adapter_config.DNSServerSearchOrder,
+            )
+
         return output
+       
 
     def get_adapter_config(self, adapter_name: str) -> AdapterInfo | None:
         output = self.get_adapters().get(adapter_name)
